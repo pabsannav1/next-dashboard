8707d4a5621c0abf844515bff2ced53eaacbb7d1
Pablo <pabsannav1@gmail.com>
2025-02-19 19:11:20 +0100

    chapter 13

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,19 +2,19 @@ import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchCardData } from '@/app/lib/data';
+// import { fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
  
 export default async function Page() {
   /* const revenue = await fetchRevenue(); */
   /* const latestInvoices = await fetchLatestInvoices(); */
-  const {
+  /* const {
     numberOfCustomers,
     numberOfInvoices,
     totalPaidInvoices,
     totalPendingInvoices,
-  } = await fetchCardData();
+  } = await fetchCardData(); */
 
   return (
     <main>
